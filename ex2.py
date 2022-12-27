8da92cffb28985fcc723e393ae5cf4ba71b865bb
funkey7dan <72495653+funkey7dan@users.noreply.github.com>
2022-12-27 11:20:29 +0200

    Added drawing of graph via code

--- ex2.py
+++ ex2.py
@@ -1,38 +1,53 @@
 import math
 from pprint import pprint
-from itertools import cycle, islice
+from itertools import cycle,islice
 import numpy
 
-
 def myData():
-    web = [{'URL': 'animals.com', 'tokens': ['jaguar', 'mammal', 'felidae', 'family', 'food'],
-            'linksTo': ['pets.org', 'nature.net']},
-           {'URL': 'pets.org', 'tokens': ['jaguar', 'cute', 'big', 'cat', 'jaguar', 'my', 'favorite'],
+    web = [{'URL': 'animals.com','tokens': ['jaguar','mammal','felidae','family','food'],
+            'linksTo': ['pets.org','nature.net']},
+           {'URL': 'pets.org','tokens': ['jaguar','cute','big','cat','jaguar','my','favorite'],
             'linksTo': ['animals.com']},
-           {'URL': 'nature.net', 'tokens': ['trees', 'plants', 'flowers', 'nature', 'outdoors'],
-            'linksTo': ['animals.com', 'hiking.info']},
-           {'URL': 'hiking.info', 'tokens': ['hiking', 'trails', 'outdoors', 'nature'],
-            'linksTo': ['nature.net', 'camping.com']},
-           {'URL': 'camping.com', 'tokens': ['camping', 'tents', 'outdoors', 'nature'],
-            'linksTo': ['hiking.info', 'travel.net']},
-           {'URL': 'travel.net', 'tokens': ['travel', 'vacation', 'destinations', 'adventure', 'favorite'],
-            'linksTo': ['camping.com', 'airlines.info']},
-           {'URL': 'airlines.info', 'tokens': ['flights', 'airlines', 'travel', 'vacation', 'favorite'],
+           {'URL': 'nature.net','tokens': ['trees','plants','flowers','nature','outdoors'],
+                                         'linksTo': ['animals.com','hiking.info']},
+           {'URL': 'hiking.info','tokens': ['hiking','trails','outdoors','nature'],
+            'linksTo': ['nature.net','camping.com']},
+           {'URL': 'camping.com','tokens': ['camping','tents','outdoors','nature'],
+            'linksTo': ['hiking.info','travel.net']},
+           {'URL': 'travel.net','tokens': ['travel','vacation','destinations','adventure','favorite'],
+            'linksTo': ['camping.com','airlines.info']},
+           {'URL': 'airlines.info','tokens': ['flights','airlines','travel','vacation','favorite'],
             'linksTo': ['travel.net']},
-           {'URL': 'recipes.com', 'tokens': ['recipes', 'cooking', 'food', 'dinner', 'breakfast', 'lunch'],
+           {'URL': 'recipes.com','tokens': ['recipes','cooking','food','dinner','breakfast','lunch'],
             'linksTo': ['animals.com']}]
     # web = [{'URL': 'example.com','tokens': ['jaguar','mammal','felidae','family'],'linksTo': ['example.org']},
     #        {'URL': 'example.org','tokens': ['jaguar','cute','big','cat','jaguar','my','favorite'],'linksTo': []},
     #        {'URL': 'example.il','tokens': ['my','cat','big'],'linksTo': ['example.com','example.org']}]
     return web
 
+def drawGraph(data):
+    import networkx as nx
+    import matplotlib.pyplot as plt
+    plt.rcParams["figure.figsize"] = (7,7)
+    # Build the graph from the data
+    G = nx.DiGraph()
+    for entry in data:
+        url = entry['URL']
+        linksTo = entry['linksTo']
+        for neighbor in linksTo:
+            G.add_edge(url,neighbor)
+
+    # Draw the graph
+    nx.draw(G,with_labels = True,font_weight = 'bold',node_size = 2500,font_size=7,arrowsize=10)
+    #plt.show()
+    plt.savefig('graph.png')
 
 def mySearchString():
-    search_string = ['jaguar', 'family', 'nature', 'travel']
+    search_string = ['jaguar','family','nature','travel']
+    #search_string = ['jaguar','family']
     return search_string
 
-
-def invertedIndex(data, searchString):
+def invertedIndex(data,searchString):
     # The keys of the dictionary will be all the tokens in searchString, and the value for each token will be a list
     # of lists
     out_dict = {s: [] for s in searchString}
@@ -44,49 +59,45 @@ def invertedIndex(data, searchString):
                 occurrences = sum(t in d['tokens'] for d in data)
                 idf = math.log2(len(url_dict['tokens']) / occurrences)
 
-                out_dict[t].append([url_dict['URL'], round(tf * idf, 3)])
+                out_dict[t].append([url_dict['URL'],round(tf * idf,3)])
 
     # Sort the lists in the inverted index by descending tf-idf score
     for token in out_dict:
-        out_dict[token].sort(key=lambda x: x[1], reverse=True)
+        out_dict[token].sort(key = lambda x: x[1],reverse = True)
 
     return out_dict
 
-
-def pageRankSimulation(data, numIter, beta):
+def pageRankSimulation(data,numIter,beta):
     # creating matrix
     amount_of_websites = len(data)
 
-    adjacency_matrix = numpy.zeros((amount_of_websites, amount_of_websites))
-    for i, website_i in enumerate(data):
-        for j, website_j in enumerate(data):
+    adjacency_matrix = numpy.zeros((amount_of_websites,amount_of_websites))
+    for i,website_i in enumerate(data):
+        for j,website_j in enumerate(data):
             website_i_outgoing_links = data[i]['linksTo']
             if website_j['URL'] in website_i_outgoing_links:
                 # if i -> j
-                adjacency_matrix[i, j] = 1 / len(website_i_outgoing_links)
+                adjacency_matrix[i,j] = 1 / len(website_i_outgoing_links)
 
-    # creating dump matrix
-    dump_matrix = numpy.full((amount_of_websites, amount_of_websites), 1 / amount_of_websites)
+    # creating damp matrix
+    dump_matrix = numpy.full((amount_of_websites,amount_of_websites),1 / amount_of_websites)
 
     # the matrix we multiply
     A = beta * adjacency_matrix + (1 - beta) * dump_matrix
 
-    importance_vector = numpy.full((amount_of_websites,), 1 / amount_of_websites)
+    importance_vector = numpy.full((amount_of_websites,),1 / amount_of_websites)
     # performing the iterations
     for i in range(numIter):
         importance_vector = A.dot(importance_vector)
 
     output_list = []
-    for i, website in enumerate(data):
-        output_list.append([website['URL'], importance_vector[i]])
+    for i,website in enumerate(data):
+        output_list.append([website['URL'],importance_vector[i]])
 
     return output_list
 
-
-def score(tfIdf, pageRankValue):
-    # TODO: change
-    return sum([tfIdf, pageRankValue])
-
+def score(tfIdf,pageRankValue):
+    return 0.7 * tfIdf + 0.3 * pageRankValue
 
 def roundrobin(*iterables):
     """roundrobin('ABC', 'D', 'EF') --> A D E B F C"""
@@ -99,19 +110,17 @@ def roundrobin(*iterables):
         except StopIteration:
             # Remove the iterator we just exhausted from the cycle.
             num_active -= 1
-            nexts = cycle(islice(nexts, num_active))
+            nexts = cycle(islice(nexts,num_active))
 
-
-def top1(invertedIndex, pageRank):
+def top1(invertedIndex,pageRank):
     seen = set()
 
     raise NotImplementedError
 
-
 def main():
-    pprint(invertedIndex(myData(), mySearchString()))
-    pprint(pageRankSimulation(myData(), 100000, 0.8))
-
+    drawGraph(myData())
+    pprint(invertedIndex(myData(),mySearchString()))
+    pprint(pageRankSimulation(myData(),100000,0.8))
 
 if __name__ == "__main__":
     main()
